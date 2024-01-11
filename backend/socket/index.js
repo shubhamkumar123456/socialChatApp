de566817f6c1839574084e40e15aff0874ba0275
shubham kumar <clboy768@gmail.com>
2024-01-11 08:34:29 +0530

    updated

--- backend/socket/index.js
+++
@@ -1,9 +0,0 @@
-// const io= require('socket.io')(8900,{
-//     cors:{
-//         origin:"http://localhost:3000"
-//     },
-// });
-
-// io.on('connection', (socket)=>{
-//     console.log('a user connected')
-// })
